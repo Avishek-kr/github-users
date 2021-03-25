83eb480825a8ae9b588163193dc159596a545612
Avishek-kr <ak251997@gmail.com>
2021-03-25 22:15:27 +0530

    show github profile amd repository

---
+++ src/3-useEffect-fetch-data.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const url = 'https://api.github.com/users';
+
+const UseEffectSecondArgument = () => {
+  const [users, setUsers] = useState([]);
+
+  const getUsers= async () => {
+    const response = await fetch(url)
+    const users = await response.json();
+    setUsers(users);
+  };
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+      {
+        users.map((user)=>{
+          const {id,login,avatar_url,followers_url, html_url}= user;
+          return(
+            <li key={id}>
+              <img src={avatar_url} alt={login}/>
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+                <h5><a href={`${html_url}?tab=repositories`}>Repositories</a></h5>
+              </div>
+            </li>
+          )
+        })
+      }
+
+      </ul>
+    </>
+  ); 
+};
+
+export default UseEffectSecondArgument;
---
+++ src/components/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Av$hek',
+      username: 'Avishek2021',
+      email: 'krbittu761@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'Vivek',
+      username: 'viv20201',
+      email: 'vivek1999@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'Kapil',
+      username: 'KapiRunner',
+      email: 'kapildev@gmail.com'
+    },
+    {
+      id: 4,
+      name: 'Dheeraj',
+      username: 'photographer',
+      email: 'dheerajKTM@gmail.com'
+    },
+    {
+      id: 5,
+      name: 'Manish',
+      username: 'manishSadLove',
+      email: 'manish143@gmail.com'
+    },
+    {
+      id: 6,
+      name: 'Aarti',
+      username: 'dabanngAarti',
+      email: 'callMeLadyDon@gmail.com'
+    },
+    {
+      id: 7,
+      name: 'Aashi',
+      username: 'doubledabanng',
+      email: 'sameAsAarti@gmail.com'
+    },
+    {
+      id: 8,
+      name: 'LoverBoy',
+      username: 'BeMyLady',
+      email: 'loveEveryone.com'
+    },
+    {
+      id: 9,
+      name: 'John Cena',
+      username: 'youCannotSeeMe',
+      email: 'seeme@gmail.com'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
